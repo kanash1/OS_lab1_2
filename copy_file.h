96ee7f047898f785eeb389e8fa062a7044614aad
kanash1 <oleg3038962@gmail.com>
2021-10-05 02:30:59 +0300

    refactoring and added the ability to copy files of any size

--- copy_file.h
+++ copy_file.h
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <string>
+#include <cstdint>
 #include <functional>
 #include <windows.h>
 #include <timeapi.h>
@@ -13,31 +14,48 @@
 
 #include "input_helper.h"
 
+class Filesize {
+public:
+	Filesize() : low_order(0), high_order(0) {}
+	Filesize(const HANDLE& handle) { low_order = GetFileSize(handle, &high_order); }
+	void set_size(const HANDLE& handle) { low_order = GetFileSize(handle, &high_order); }
+	DWORD get_low() const { return low_order; }
+	DWORD get_high() const { return high_order; }
+	uint64_t get_fullsize() const { return (static_cast<uint64_t>(high_order) << 32) + static_cast<uint64_t>(low_order); } // don't work with LP64
+private:
+	DWORD low_order;
+	DWORD high_order;
+};
+
 size_t callback_count = 0;
 
 VOID CALLBACK FileIOCompletionRoutine(DWORD error, DWORD number_of_bytes, LPOVERLAPPED p_over) { ++callback_count; }
 
-void action_file_overlapped(
+void action_overlapped(
 	const std::function<BOOL(HANDLE, LPVOID, DWORD, LPOVERLAPPED, LPOVERLAPPED_COMPLETION_ROUTINE)>& func,
 	const HANDLE& file_handle,
 	const size_t& block_size,
 	const size_t& operations_count,
-	int l_file_size,
-	BYTE**& buffer,
+	const Filesize& filesize,
+	uint8_t**& buffer,
 	OVERLAPPED*& over
 ) {
 	size_t s_operations_count = 0;
+	uint64_t current_size = filesize.get_fullsize();
+	uint64_t shift = static_cast<uint64_t>(block_size) * static_cast<uint64_t>(operations_count);
 	for (size_t i = 0; i < operations_count; ++i) {
-		if (l_file_size > 0) {
-			++s_operations_count;
-			func(file_handle, buffer[i], block_size, &over[i], FileIOCompletionRoutine);
-			l_file_size -= block_size;
-		}
+		++s_operations_count;
+		func(file_handle, buffer[i], static_cast<DWORD>(block_size), &over[i], FileIOCompletionRoutine);
+		if (current_size <= static_cast<uint64_t>(block_size))
+			break;
+		current_size -= static_cast<uint64_t>(block_size);
 	}
 	while (callback_count < s_operations_count)
-		SleepEx(SIZE_MAX, true);
-	for (size_t i = 0; i < operations_count; i++)
-		over[i].Offset += block_size * operations_count;
+		SleepEx(static_cast<DWORD>(std::numeric_limits<size_t>::max()), true);
+	for (size_t i = 0; i < operations_count; i++) {
+		over[i].Offset += static_cast<DWORD>((shift << 32) >> 32);
+		over[i].OffsetHigh += static_cast<DWORD>(shift >> 32);
+	}
 	callback_count = 0;
 }
 
@@ -46,34 +64,43 @@ void copy_file(
 	const HANDLE& copy_handle,
 	const size_t& block_size,
 	const size_t& operations_count,
-	int l_file_size,
-	BYTE**& buffer,
+	const Filesize& filesize,
+	uint8_t**& buffer,
 	OVERLAPPED*& over_read,
 	OVERLAPPED*& over_write
 ) {
-	int current_size = l_file_size;
+	uint64_t current_size = filesize.get_fullsize();
+	uint64_t shift = static_cast<uint64_t>(block_size * operations_count);
 	do {
-		action_file_overlapped(ReadFileEx, source_handle, block_size, operations_count, l_file_size, buffer, over_read);
-		action_file_overlapped(WriteFileEx, copy_handle, block_size, operations_count, l_file_size, buffer, over_write);
-		current_size -= block_size * operations_count;
-	} while (current_size > 0);
+		if (shift > current_size)
+			shift = current_size;
+		action_overlapped(ReadFileEx, source_handle, block_size, operations_count, filesize, buffer, over_read);
+		action_overlapped(WriteFileEx, copy_handle, block_size, operations_count, filesize, buffer, over_write);
+		current_size -= shift;
+	} while (current_size != 0);
 }
 
-LONGLONG time_calculation(const LARGE_INTEGER& start, const LARGE_INTEGER& end, const LARGE_INTEGER& freq) {
-	LARGE_INTEGER res;
-	res.QuadPart = end.QuadPart - start.QuadPart;
-	res.QuadPart *= 1000000;
-	res.QuadPart /= freq.QuadPart;
-	return res.QuadPart;
+uint64_t time_calculation(const LARGE_INTEGER& start, const LARGE_INTEGER& end, const LARGE_INTEGER& freq) {
+	return ((end.QuadPart - start.QuadPart) * 1000000) / freq.QuadPart;
 }
 
 void process_copy() {
+	OVERLAPPED* over_read = nullptr;
+	OVERLAPPED* over_write = nullptr;
+	uint8_t** buffer = nullptr;	
+	long tmp = 0;
+	size_t block_size = 0;
+	size_t operations_count = 0;
+	Filesize filesize;
+	HANDLE source_handle;
+	HANDLE copy_handle;
+	InputError error;
 	std::string source_path;
 	std::string copy_path;
 
 	std::cout << "Enter source path:\n";
 	std::getline(std::cin, source_path);
-	HANDLE source_handle = CreateFile(source_path.c_str(), GENERIC_READ, 0, nullptr,
+	source_handle = CreateFile(source_path.c_str(), GENERIC_READ, 0, nullptr,
 		OPEN_EXISTING, FILE_FLAG_NO_BUFFERING | FILE_FLAG_OVERLAPPED, NULL);
 	if (source_handle == INVALID_HANDLE_VALUE) {
 		std::cout << "Error occurred while openning the file. Error code: " << GetLastError() << '\n';
@@ -82,7 +109,7 @@ void process_copy() {
 
 	std::cout << "Enter copy path:\n";
 	std::getline(std::cin, copy_path);
-	HANDLE copy_handle = CreateFile(copy_path.c_str(), GENERIC_WRITE, 0, nullptr,
+	copy_handle = CreateFile(copy_path.c_str(), GENERIC_WRITE, 0, nullptr,
 		CREATE_ALWAYS, FILE_FLAG_NO_BUFFERING | FILE_FLAG_OVERLAPPED, NULL);
 	if (copy_handle == INVALID_HANDLE_VALUE && GetLastError() != 0) {
 		std::cout << "Error occurred while creating the file. Error code: " << GetLastError() << '\n';
@@ -90,47 +117,46 @@ void process_copy() {
 		return;
 	}
 
-	size_t block_size;
 	std::cout << "Enter block size:\n";
-	InputError error = input(std::cin, block_size);
+	error = input(std::cin, block_size);
 	if (error != InputError::ERROR_NORMAL) {
 		std::cout << "Invalid input\n";
+		CloseHandle(source_handle);
+		CloseHandle(copy_handle);
 		return;
 	}
 	block_size *= 4096;
 
-	size_t operations_count;
 	std::cout << "Enter operations count:\n";
 	error = input(std::cin, operations_count);
 	if (error != InputError::ERROR_NORMAL) {
 		std::cout << "Invalid input\n";
+		CloseHandle(source_handle);
+		CloseHandle(copy_handle);
 		return;
 	}
 
-	int l_source_size = 0;
-	DWORD h_source_size = 0;
-	l_source_size = GetFileSize(source_handle, &h_source_size);
-	BYTE** buffer = new BYTE * [operations_count];
-	for (size_t i = 0; i < operations_count; ++i)
-		buffer[i] = new BYTE[block_size];
-	OVERLAPPED* over_read = new OVERLAPPED[operations_count];
-	OVERLAPPED* over_write = new OVERLAPPED[operations_count];
+	filesize.set_size(source_handle);
+	buffer = new uint8_t * [operations_count];
+	over_read = new OVERLAPPED[operations_count];
+	over_write = new OVERLAPPED[operations_count];
 	for (size_t i = 0; i < operations_count; ++i) {
-		over_write[i].Offset = i * block_size;
-		over_write[i].OffsetHigh = i * h_source_size;
-		over_read[i].Offset = over_write[i].Offset;
-		over_read[i].OffsetHigh = over_write[i].OffsetHigh;
+		uint64_t shift = static_cast<uint64_t>(i) * static_cast<uint64_t>(block_size);
+		over_read[i].Offset = over_write[i].Offset = static_cast<DWORD>((shift << 32) >> 32);
+		over_read[i].OffsetHigh = over_write[i].OffsetHigh = static_cast<DWORD>(shift >> 32);
+		buffer[i] = new uint8_t[block_size];
 	}
 
 	LARGE_INTEGER start_time, end_time, frequency;
 	QueryPerformanceFrequency(&frequency);
 	QueryPerformanceCounter(&start_time);
-	copy_file(source_handle, copy_handle, block_size, operations_count, l_source_size, buffer, over_read, over_write);
+	copy_file(source_handle, copy_handle, block_size, operations_count, filesize, buffer, over_read, over_write);
 	QueryPerformanceCounter(&end_time);
 
 	std::cout << "Time: " << time_calculation(start_time, end_time, frequency) << " microseconds\n";
 
-	SetFilePointer(copy_handle, l_source_size, nullptr, FILE_BEGIN);
+	tmp = filesize.get_high();
+	SetFilePointer(copy_handle, filesize.get_low(), &tmp, FILE_BEGIN);
 	SetEndOfFile(copy_handle);
 	CloseHandle(source_handle);
 	CloseHandle(copy_handle);
